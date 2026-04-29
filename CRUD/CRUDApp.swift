8ed0a077ca8820fda7304b9dd65e1685f4e227a3
mrpedroox <pedrohenriquetavares06@gmail.com>
2026-04-29 18:09:02 -0300

    commit pra iniciar

---
+++ CRUD/CRUDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CRUDApp.swift
+//  CRUD
+//
+//  Created by Turma01-17 on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct CRUDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CRUD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CRUD
+//
+//  Created by Turma01-17 on 29/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
